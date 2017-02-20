84af11b0ae5a1b7e0228d8d9a1468ed8de85f06c
Timothy Brennan <2timothy215@gmail.com>
2017-02-20 13:01:14 -0500

    Fix icon images

--- wp-content/themes/redt/templates/head.php
+++ wp-content/themes/redt/templates/head.php
@@ -1,27 +1,23 @@
-<?php
-  use Roots\Sage\Assets;
-?>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon.png') ?>">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-57x57.png') ?>" sizes="57x57">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-60x60.png') ?>" sizes="60x60">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-72x72.png') ?>" sizes="72x72">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-76x76.png') ?>" sizes="76x76">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-114x114.png') ?>" sizes="114x114">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-120x120.png') ?>" sizes="120x120">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-144x144.png') ?>" sizes="144x144">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-152x152.png') ?>" sizes="152x152">
-  <link rel="apple-touch-icon" href="<?= Assets\asset_path('images/apple-icon-180x180.png') ?>" sizes="180x180">
-  <link rel="icon" href="<?= Assets\asset_path('images/android-icon-36x36.png') ?>" sizes="36x36">
-  <link rel="icon" href="<?= Assets\asset_path('images/android-icon-48x48.png') ?>" sizes="48x48">
-  <link rel="icon" href="<?= Assets\asset_path('images/android-icon-72x72.png') ?>" sizes="72x72">
-  <link rel="icon" href="<?= Assets\asset_path('images/android-icon-96x96.png') ?>" sizes="96x96">
-  <link rel="icon" href="<?= Assets\asset_path('images/android-chrome-144x144.png') ?>" sizes="144x144">
-  <link rel="icon" href="<?= Assets\asset_path('images/android-chrome-192x192.png') ?>" sizes="192x192">
-  <link rel="icon" href="<?= Assets\asset_path('images/favicon.ico') ?>">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon.png'; ?>">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-57x57.png'; ?>" sizes="57x57">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-60x60.png'; ?>" sizes="60x60">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-72x72.png'; ?>" sizes="72x72">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-76x76.png'; ?>" sizes="76x76">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-114x114.png'; ?>" sizes="114x114">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-120x120.png'; ?>" sizes="120x120">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-144x144.png'; ?>" sizes="144x144">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-152x152.png'; ?>" sizes="152x152">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri().'/assets/images/apple-icon-180x180.png'; ?>" sizes="180x180">
+  <link rel="icon" href="<?= get_stylesheet_directory_uri().'/assets/images/android-icon-36x36.png'; ?>" sizes="36x36">
+  <link rel="icon" href="<?= get_stylesheet_directory_uri().'/assets/images/android-icon-48x48.png'; ?>" sizes="48x48">
+  <link rel="icon" href="<?= get_stylesheet_directory_uri().'/assets/images/android-icon-72x72.png'; ?>" sizes="72x72">
+  <link rel="icon" href="<?= get_stylesheet_directory_uri().'/assets/images/android-icon-96x96.png'; ?>" sizes="96x96">
+  <link rel="icon" href="<?= get_stylesheet_directory_uri().'/assets/images/android-chrome-144x144.png'; ?>" sizes="144x144">
+  <link rel="icon" href="<?= get_stylesheet_directory_uri().'/assets/images/android-chrome-192x192.png'; ?>" sizes="192x192">
+  <link rel="icon" href="<?= get_stylesheet_directory_uri().'/assets/images/favicon.ico'; ?>">
   <?php wp_head(); ?>
-  <?= get_field('scripts_head', 'option'); ?>
 </head>
