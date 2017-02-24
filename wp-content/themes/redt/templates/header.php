25966791976e3fc3373eaf0f9ce8bed65b6b2227
Timothy Brennan <2timothy215@gmail.com>
2017-02-24 10:43:51 -0500

    Header start

--- wp-content/themes/redt/templates/header.php
+++ wp-content/themes/redt/templates/header.php
@@ -1,12 +1,36 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <div class="row">
+      <div class="navbar-header col-xs-12 col-md-9">
+        <nav class="nav-primary">
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          endif;
+          ?>
+        </nav>
+      </div>
+      <div class="col-xs-0 col-md-3 col-centered search-container">
+        <?php get_search_form();
+          //get_template_part('templates/search');
+        ?>
+       </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+          <?php
+            if ( get_theme_mod('theme_logo') ) :
+              echo '<img src="' . esc_url( get_theme_mod('theme_logo') ) . '" class="img-responsive">';
+            else:
+              echo get_bloginfo('name') . '<span>' . get_bloginfo('description') . '</span>';
+            endif;
+          ?>
+          </a>
+      </div>
+      <div class="col-md-9">
+
+      </div>
+    </div>
   </div>
 </header>
---
+++ wp-content/themes/redt/templates/search.php
@@ -0,0 +1,5 @@
+	<form id="search" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+        <div id="label"><label for="search-terms" id="search-label">search</label></div>
+        <div id="input"><input type="text" name="s" id="search-terms" placeholder="Search Real Estate Deal Talk">
+        	<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>
+    </form>
